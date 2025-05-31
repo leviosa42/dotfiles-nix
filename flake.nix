61203705566093a1b27bb6aa41e628d293a3e4f8
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-06-01 01:32:03 +0900

    fix: pre-commit

--- flake.nix
+++ flake.nix
@@ -72,9 +72,7 @@
 
       devShells = {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            pre-commit
-          ];
+          inputsFrom = [ config.pre-commit.devShell ];
         };
       };
     };
