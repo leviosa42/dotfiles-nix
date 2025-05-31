c8bbf40233927bcceaa3ba7ac3d07f087f29a921
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2025-06-01 01:18:29 +0900

    wip: pre-commit

--- flake.nix
+++ flake.nix
@@ -1,56 +1,82 @@
 {
-  description = "Home Manager configuration of motch";
-
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # flake-utils.url = "github:numtide/flake-utils";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    systems.url = "github:nix-systems/default";
+
+    # treefmt-nix = {
+    #   url = "github:numtide/treefmt-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      home-manager,
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    flake-parts,
+    home-manager,
+    systems,
+    ...
+  }:
+  flake-parts.lib.mkFlake {inherit inputs; } {
+    systems = import systems;
+
+    imports = [
+      ## REF: https://flake.parts/options/git-hooks-nix.html
+      inputs.git-hooks-nix.flakeModule
+    ];
+
+    perSystem = {
+      config,
+      pkgs,
+      system,
       ...
-    }:
-    let
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      nixosConfigurations."nixos" = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users."motch" = import ./nix/home-manager;
-          }
-        ];
+    }: {
+      pre-commit = {
+        check.enable = true; # perform the pre-commit checks in `nix flake check`
+        settings = {
+          # src = ./.;
+          ## REF: https://flake.parts/options/git-hooks-nix.html#opt-perSystem.pre-commit.settings.hooks
+          hooks = {
+            ## --- predefined hooks ---
+            deadnix = { # Scan Nix files for dead code (unused variables bindings)
+              enable = true;
+              settings = {
+                noLambdaPatternNames = true; # Don’t check lambda pattern names (don’t break nixpkgs callPackage).
+              };
+            };
+            # detect-private-keys.enable = true; # Detect the presence of private keys
+            fix-byte-order-marker.enable = true; # Remove UTF-8 BOM
+            flake-checker.enable = true; # Run health checks on flake-powered Nix projects
+            # nixfmt-rfc-style.enable = true; # Format nixfmt-rfc-style
+            # shellcheck.enable = true; # Format shell scripts
+            # statix.enable = true; # Lints and suggests improvements to Nix code
+            ## REF: https://flake.parts/options/git-hooks-nix.html#opt-perSystem.pre-commit.settings.hooks.treefmt
+            # treefmt.enable = true;
+            ## --- custom hooks ---
+          };
+        };
       };
 
-      homeConfigurations."rpi5" = home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages."aarch64-linux";
-        modules = [ ./nix/home-manager ];
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            pre-commit
+          ];
+        };
       };
-
-      homeConfigurations."wsl" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-        modules = [ ./nix/home-manager ];
-      };
-
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
-
     };
+  };
 }
