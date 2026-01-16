7ac9b285a726f24a38b2b48a1ce25b8cbe294043
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2026-01-16 15:53:57 +0900

    build: flake.lock

--- nix/machines/NixOS-Desktop/configuration.nix
+++ nix/machines/NixOS-Desktop/configuration.nix
@@ -106,7 +106,7 @@
     packages = with pkgs; [
       noto-fonts-cjk-serif
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       #nerdfonts
       plemoljp-nf
     ];
