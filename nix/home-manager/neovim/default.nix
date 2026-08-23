f986ed8fa8dc83ca9893493b61db5dc945b4ff4f
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2026-08-23 23:25:12 +0900

    build(flake,neovim): update flake.lock to 26.05

--- nix/home-manager/neovim/default.nix
+++ nix/home-manager/neovim/default.nix
@@ -10,6 +10,9 @@ in
     withPython3 = false;
     withRuby = false;
     # extraLuaConfig = builtins.readFile ./init.lua;
+    # REF: https://blog-a2e.pages.dev/p/2026%E5%B9%B4-6%E6%9C%88-%E7%AC%AC%E4%BA%8C%E9%80%B1/
+    # use sideloadInitLua = true; for using symlinking to nvim dir
+    sideloadInitLua = true;
     extraPackages = with pkgs; [
       websocat # for typst-preview
       nil
